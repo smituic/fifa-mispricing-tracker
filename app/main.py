db628ebb6d58010946362fb91021944493bf9d14
Smit Patel <spate841@uic.edu>
2026-03-04 18:39:20 -0600

    Changed main file to connect to frontend

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import asyncio
+from fastapi.middleware.cors import CORSMiddleware
 from app.services.snapshot_service import init_db, start_snapshot_loop
 from app.api.routes.kalshi import router as kalshi_router
 
@@ -7,7 +8,13 @@ app = FastAPI(title="FIFA Mispricing Tracker", version="0.1.0")
 
 app.include_router(kalshi_router, prefix="/kalshi", tags=["kalshi"])
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "FIFA Mispricing Tracker is running 🚀"}
